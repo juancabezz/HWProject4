f3c019a97ff9ad7f729c2291320ac8b088e7e7a6
juancabeza <juancabeza@localhost>
2023-07-28 23:14:55 -0400

    Initial Commit

---
+++ src/soccer/SoccerTeam.java
@@ -0,0 +1,11 @@
+package soccer;
+
+public class SoccerTeam {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My favorite soccer team is Independiente Santa Fe from Colombia");
+		System.out.println("I love to go to the stadium every time I go to Colombia");
+	}
+
+}
